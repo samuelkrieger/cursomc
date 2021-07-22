114a7b633c379ce01a34c36f518c91c8d5ea5634
Samuel Krieger Bonini <samuel.kriegerb@gmail.com>
2021-07-21 23:22:39 -0300

    primeiro commit

---
+++ src/main/java/br/com/samuel/cursomc/resources/CategoriaResource.java
@@ -0,0 +1,32 @@
+package br.com.samuel.cursomc.resources;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.samuel.cursomc.domain.Categoria;
+import br.com.samuel.cursomc.services.CategoriaService;
+
+@RestController
+@RequestMapping(value="/categorias")
+public class CategoriaResource {
+	
+	@Autowired
+	private CategoriaService  service;
+	
+	
+	
+	@RequestMapping(value = "/{id}",method = RequestMethod.GET)
+	public ResponseEntity<Optional<Categoria>> find(@PathVariable Integer id){
+		Optional<Categoria>  categoria = service.buscar(id);
+		
+		return ResponseEntity.ok(categoria);
+		
+		
+	}
+}
---
+++ src/main/java/br/com/samuel/cursomc/services/CategoriaService.java
@@ -0,0 +1,23 @@
+package br.com.samuel.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.samuel.cursomc.domain.Categoria;
+import br.com.samuel.cursomc.repositories.CategoriaRepository;
+
+@Service
+public class CategoriaService {
+	
+	@Autowired
+	private CategoriaRepository repository;
+
+	public Optional<Categoria> buscar(Integer id) {
+		Optional<Categoria> obj = repository.findById(id);
+		
+		return obj;
+		
+	}
+}
