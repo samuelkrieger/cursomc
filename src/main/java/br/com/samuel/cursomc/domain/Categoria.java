cf96282ad7e928882149342474774ec20540b747
Samuel Krieger Bonini <samuel.kriegerb@gmail.com>
2021-07-27 22:02:34 -0300

    domain e repository alterados

--- src/main/java/br/com/samuel/cursomc/domain/Categoria.java
+++ src/main/java/br/com/samuel/cursomc/domain/Categoria.java
@@ -1,11 +1,14 @@
 package br.com.samuel.cursomc.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToMany;
 
 @Entity
 public class Categoria implements Serializable {
@@ -18,6 +21,10 @@ public class Categoria implements Serializable {
 
 	private String nome;
 
+	@ManyToMany(mappedBy = "categorias")
+	private List<Produto> produtos = new ArrayList<>();
+	
+	
 	public Categoria() {
 
 	}
